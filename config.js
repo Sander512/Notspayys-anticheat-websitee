4c08275449d6391a732d85eae888ca1350182575
Notspay <148631539+Sander512@users.noreply.github.com>
2026-09-21 08:51:14 +0200

    Remove trailing slash from API_BASE_URL

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.NOTSPAYYS_CONFIG = {
-    API_BASE_URL: 'https://notspayy-anticheat-api.vercel.app/',
+    API_BASE_URL: 'https://notspayy-anticheat-api.vercel.app',
     DISCORD_INVITE_URL: 'https://discord.gg/8NUnKJgsMh',
 };
